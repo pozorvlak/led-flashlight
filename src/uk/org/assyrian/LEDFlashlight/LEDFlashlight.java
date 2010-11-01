c6c9185a0508742ee91197616f734ef5c3953ecf
Miles Gould <miles@assyrian.org.uk>
2010-11-01 17:06:49 +0000

    Added some logging.

--- src/uk/org/assyrian/LEDFlashlight/LEDFlashlight.java
+++ src/uk/org/assyrian/LEDFlashlight/LEDFlashlight.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class LEDFlashlight extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	// Log.d("LEDFlashlight", "entered onCreate");
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         this.onOffButton = (Button) this.findViewById(R.id.onoff);
@@ -38,14 +40,17 @@ public class LEDFlashlight extends Activity {
     }
 
     private boolean turnFlashOff() {
+    	// Log.d("LEDFlashlight", "Turning flash off");
     	mCamera.stopPreview();
     	Parameters p = mCamera.getParameters();
     	p.setFlashMode(mFlashMode);
     	mCamera.setParameters(p);
+    	Log.d("LEDFlashlight", "Turned flash off");
 		return false;
 	}
 
 	private boolean turnFlashOn() {
+		// Log.d("LEDFlashlight", "Turning flash on");
 		Parameters p = mCamera.getParameters();
 		List<String> flashModes	= p.getSupportedFlashModes();
 		if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
@@ -53,19 +58,24 @@ public class LEDFlashlight extends Activity {
 			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
 			mCamera.setParameters(p);
 			mCamera.startPreview();
+			Log.d("LEDFlashlight", "Turned flash on");
 			return true;
 		}
+		Log.d("LEDFlashlight", "FLASH_MODE_TORCH not supported");
 		return false;
 	}
 	
 	@Override
 	protected void onPause() {
+		// Log.d("LEDFlashlight", "Pausing");
 		super.onPause();
 		mCamera.release();
+		// Log.d("LEDFlashlight", "Leaving onPause");
 	}
 	
 	@Override
 	protected void onResume() {
+		// Log.d("LEDFlashlight", "Entering onResume");
 		super.onResume();
 		mCamera = Camera.open();
 	}
